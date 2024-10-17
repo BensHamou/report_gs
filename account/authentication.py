b483ee28cfa9f759b9df9a699929ce5647636a45
mohammed.benslimane <benshamou@gmail.com>
2024-10-17 08:57:01 +0100

    Initial commit

---
+++ account/authentication.py
@@ -0,0 +1,40 @@
+import requests
+from django.contrib.auth.backends import BaseBackend
+from .models import User
+from requests.auth import HTTPBasicAuth
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages 
+from django.db.models import Q
+
+
+class ApiBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            if username in ['admin', 'admin@admin.com']:
+                if check_password(password, user.password):
+                    return user
+                else:
+                    messages.error(request, "LOGIN : Mot de passe incorrect.")
+                    return None
+            
+            auth = HTTPBasicAuth(user.email, password)
+
+            response = requests.post('https://api.ldap.groupe-hasnaoui.com/pumaprd/auth', auth=auth)
+
+            if not response.status_code == 200:
+                messages.error(request, "LOGIN : Problème avec la connexion au serveur.")
+            else:
+                if not response.json().get('authenticated'):
+                    messages.error(request, "LOGIN : Mot de passe incorrect.")
+                else:
+                    return user
+        except User.DoesNotExist:
+            messages.error(request, "LOGIN : Utilisateur pas trouvé.")
+        return None
+    
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
---
+++ report_gs/settings.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+AUTHENTICATION_BACKENDS = [
+    'account.authentication.ApiBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    ]
+
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
+
+DEBUG = True
+
+swappable = 'AUTH_USER_MODEL'
+
+AUTH_USER_MODEL = 'account.User'
+
+ADMIN_URL = 'puma_gs/admin/'
+
+ALLOWED_HOSTS = ['*']
+
+CSRF_COOKIE_SECURE = False
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "account",
+    "report",
+    "bootstrap5",
+    "fontawesomefree",
+    "django_filters",
+    "widget_tweaks",
+    "django_extensions",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "report_gs.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "report_gs.wsgi.application"
+
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'PumaGS',
+       'USER': 'puma_gs',
+       'PASSWORD': 'puma_gs',
+       'HOST': '10.10.10.101',
+       'PORT': '5434',
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "fr"
+
+TIME_ZONE = "Africa/Algiers"
+
+USE_I18N = True
+
+USE_TZ = True
+
+LOGIN_REDIRECT_URL = 'login_success'
+LOGOUT_REDIRECT_URL = '/login'
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'login_success'
+LOGOUT_REDIRECT_URL = '/login'
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'pumatransplaninng@gmail.com'
+EMAIL_HOST_PASSWORD = 'hvywqabtuhfipwnq'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'pumatransplaninng@gmail.com'
