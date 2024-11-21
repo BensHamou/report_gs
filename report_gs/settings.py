6c17e2ed214862a03401e9ce55cee93808c100fa
mohammed.benslimane <benshamou@gmail.com>
2024-11-21 08:56:19 +0100

    correct network, settings and req

--- report_gs/settings.py
+++ report_gs/settings.py
@@ -85,14 +85,22 @@ DATABASES = {
     #    'HOST': '10.10.10.101',
     #    'PORT': '5434',
     # },
+    # 'default': {
+    #  'ENGINE': 'django.db.backends.postgresql',
+    #  'NAME': 'PumaGS',
+    #  'USER': 'puma_gs',
+    #  'PASSWORD': 'puma_gs',
+    #  'HOST': '127.0.0.1',
+    #  'PORT': '5432',
+    # },
     'default': {
-     'ENGINE': 'django.db.backends.postgresql',
-     'NAME': 'PumaGS',
-     'USER': 'puma_gs',
-     'PASSWORD': 'puma_gs',
-     'HOST': '127.0.0.1',
-     'PORT': '5432',
-    },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
 }
 
 
