c27b7244f2559879da6b497555b27d51561d6a9e
mohammed.benslimane <benshamou@gmail.com>
2024-11-28 13:00:42 +0100

    prod settings

--- report_gs/settings.py
+++ report_gs/settings.py
@@ -98,22 +98,22 @@ DATABASES = {
     #   'HOST': '10.10.10.20',
     #   'PORT': '5172',
     #},
-    'default': {
-     'ENGINE': 'django.db.backends.postgresql',
-     'NAME': 'PumaGS',
-     'USER': 'puma_gs',
-     'PASSWORD': 'puma_gs',
-     'HOST': '127.0.0.1',
-     'PORT': '5432',
-    },
     #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': os.environ.get('DB_NAME'),
-    #    'USER': os.environ.get('DB_USER'),
-    #    'PASSWORD': os.environ.get('DB_PASS'),
-    #    'HOST': os.environ.get('DB_HOST'),
-    #    'PORT': os.environ.get('DB_PORT'),
-    #}
+    # 'ENGINE': 'django.db.backends.postgresql',
+    # 'NAME': 'PumaGS',
+    # 'USER': 'puma_gs',
+    # 'PASSWORD': 'puma_gs',
+    # 'HOST': '127.0.0.1',
+    # 'PORT': '5432',
+    #},
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
 }
 
 
