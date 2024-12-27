b3ad2c66ea060c60c2659d12a50da1da68afef58
mohammed.benslimane <benshamou@gmail.com>
2024-12-27 03:50:45 +0100

    ..

--- report/models.py
+++ report/models.py
@@ -179,8 +179,7 @@ class Move(BaseModel):
                     if not ds:
                         raise ValueError(f"{detail.n_lot} - Stock introuvable dans {detail.emplacement.designation} pour le produit {ml.product}.")
                     ds.qte -= detail.qte
-                    if ds.product.qte_per_pal > detail.qte:
-                        ds.palette -= math.floor(detail.qte / ds.product.qte_per_pal)
+                    ds.palette -= math.floor(detail.qte / ds.product.qte_per_pal)
                 if ds.qte > 0:
                     ds.save()
                 else:
