d853169d8caf1fff3a9026d7e9338e015043cbd9
mohammed.benslimane <benshamou@gmail.com>
2025-02-09 16:48:35 +0100

    remove check BL

--- account/api.py
+++ account/api.py
@@ -130,12 +130,12 @@ class CreateMoveOut(APIView):
         transferred_products = request.data.get('transferred_products', [])
         n_bls = request.data.get('n_bls', [])
 
-        if move_type == 'normal' and request.user.role != 'Admin':
-            for n_bl in n_bls:
-                numero = n_bl.get('numero')
-                is_annexe = bl_data.get('is_annexe', False)
-                if MoveBL.objects.filter(numero=numero, move__site=request.user.default_site, move__date__year=datetime.today().year, is_annexe=is_annexe).exists():
-                    return Response({"detail": f"Le BL {numero} existe déjà pour ce site."}, status=400)
+        # if move_type == 'normal' and request.user.role != 'Admin':
+        #     for n_bl in n_bls:
+        #         numero = n_bl.get('numero')
+        #         is_annexe = bl_data.get('is_annexe', False)
+        #         if MoveBL.objects.filter(numero=numero, move__site=request.user.default_site, move__date__year=datetime.today().year, is_annexe=is_annexe).exists():
+        #             return Response({"detail": f"Le BL {numero} existe déjà pour ce site."}, status=400)
 
         if not transferred_products:
             return Response({"detail": "Produits manquants"}, status=400)
